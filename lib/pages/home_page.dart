6b00b67546244b96e5a732f1c33dfcf10df9f6eb
Wais Raoufi <raoufiwais@gmail.com>
2026-07-14 16:47:34 +0430

    internet connection

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -3,6 +3,7 @@ import 'package:newsapp/models/items_modal.dart';
 import '../Network/api_service.dart';
 import '../widgets/news_card.dart';
 import '../widgets/shimmer_card.dart';
+import 'package:internet_connection_checker_plus/internet_connection_checker_plus.dart';
 
 class HomePage extends StatefulWidget {
   const HomePage({super.key});
@@ -16,6 +17,17 @@ class _HomePageState extends State<HomePage> {
 
   List<ItemsModal> news = [];
   bool isLoading = true;
+  bool hasInternet = true;
+
+  Future<bool> checkInternet() async {
+    final result = await InternetConnection().hasInternetAccess;
+
+    setState(() {
+      hasInternet = result;
+    });
+
+    return result;
+  }
 
   @override
   void initState() {
@@ -24,6 +36,15 @@ class _HomePageState extends State<HomePage> {
   }
 
   Future<void> loadNews() async {
+    final internet = await checkInternet();
+
+    if (!internet) {
+      setState(() {
+        isLoading = false;
+      });
+      return;
+    }
+
     final data = await service.getNews();
     print(data);
     setState(() {
@@ -37,48 +58,59 @@ class _HomePageState extends State<HomePage> {
     return Scaffold(
       backgroundColor: Colors.grey[200],
       appBar: AppBar(
-        backgroundColor: Colors.grey[400],
+        backgroundColor: Colors.white,
         title: const Text(
           "News App",
           style: TextStyle(fontWeight: FontWeight.bold),
         ),
       ),
-      body: RefreshIndicator(
-        onRefresh: () async {
-          setState(() {
-            isLoading = true;
-          });
-          await loadNews();
-        },
-        child: isLoading
-            ? ListView.builder(
-                itemCount: 10,
-                itemBuilder: (context, index) {
-                  return ShimmerCard();
-                },
-              )
-            : ListView.builder(
-                itemCount: news.length,
-                itemBuilder: (context, index) {
-                  final item = news[index];
-                  return InkWell(
-                    onTap: () {
-                      showModalBottomSheet(
-                        context: context,
-                        isScrollControlled: true,
-                        builder: (context) {
-                          return Padding(
-                            padding: const EdgeInsets.all(20),
-                            child: Text(item.description),
-                          );
-                        },
-                      );
-                    },
-                    child: NewsCard(item: item),
-                  );
-                },
+      body: !hasInternet
+          ? Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Image.asset('assets/images.png', width: 200),
+
+                  const Text('NO Internet connection'),
+                ],
               ),
-      ),
+            )
+          : RefreshIndicator(
+              onRefresh: () async {
+                setState(() {
+                  isLoading = true;
+                });
+                await loadNews();
+              },
+              child: isLoading
+                  ? ListView.builder(
+                      itemCount: 10,
+                      itemBuilder: (context, index) {
+                        return ShimmerCard();
+                      },
+                    )
+                  : ListView.builder(
+                      itemCount: news.length,
+                      itemBuilder: (context, index) {
+                        final item = news[index];
+                        return InkWell(
+                          onTap: () {
+                            showModalBottomSheet(
+                              context: context,
+                              isScrollControlled: true,
+                              builder: (context) {
+                                return Padding(
+                                  padding: const EdgeInsets.all(20),
+                                  child: Text(item.description),
+                                );
+                              },
+                            );
+                          },
+                          child: NewsCard(item: item),
+                        );
+                      },
+                    ),
+            ),
     );
   }
 }
